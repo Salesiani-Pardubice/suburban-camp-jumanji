c7dcd46a9f01a9b15d19fa6346353f608d603d55
petrkucerak <petr@khome.cz>
2023-08-19 01:39:06 +0200

    Finish script

--- final-scene/script.sh
+++ final-scene/script.sh
@@ -4,16 +4,372 @@ key="123456789"
 
 # Colors
 RED='\033[0;31m'
+PURPURE='\033[0;35m'
+CIAN='\033[0;35m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+BLACK='\033[0;30m'
 NC='\033[0m' # No Color
 
 while
    clear
-   echo -n "Zadejte heslo: "
+
+   echo "${YELLOW}● Status: PENDING${NC}                                                     ${GREEN}Systems: 100%${NC}"
+   echo ""
+   echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+   echo "This is the Main controlling menu. This terminal has sudo permissions."
+   printf "${GREEN}
+  ____  __ __  ___ ___   ____  ____   ____  ____       ____  ____ 
+ |    ||  |  ||   |   | /    ||    \ |    ||    |     /    ||    |
+ |__  ||  |  || _   _ ||  o  ||  _  ||__  | |  |     |  o  | |  | 
+ __|  ||  |  ||  \_/  ||     ||  |  |__|  | |  |     |     | |  | 
+/  |  ||  :  ||   |   ||  _  ||  |  /  |  | |  |     |  _  | |  | 
+\  \`  ||     ||   |   ||  |  ||  |  \  \`  | |  |     |  |  | |  | 
+ \____j \__,_||___|___||__|__||__|__|\____||____|    |__|__||____|
+
+${NC}"
+
+   echo -n "$ Zadejte heslo: "
    read password
    echo
    [ "$password" != "$key" ]
 do
    clear
-   printf "Zadejte heslo: ${RED}ŠPATNÉ HESLO!${NC}"
+
+   echo "${RED}● Status: WRONG PASSWORD${NC}                                              ${GREEN}Systems: 100%${NC}"
+   echo ""
+   echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+   echo "This is the Main controlling menu. This terminal has sudo permissions."
+   printf "${RED}
+ __  __  __       _____ ____   ____  ______  ____     ___      __  __  __ 
+|  ||  ||  |     / ___/|    \ /    ||      ||    \   /  _]    |  ||  ||  |
+|  ||  ||  |    (   \_ |  o  )  o  ||      ||  _  | /  [_     |  ||  ||  |
+|__||__||__|     \__  ||   _/|     ||_|  |_||  |  ||    _]    |__||__||__|
+ __  __  __      /  \ ||  |  |  _  |  |  |  |  |  ||   [_      __  __  __ 
+|  ||  ||  |     \    ||  |  |  |  |  |  |  |  |  ||     |    |  ||  ||  |
+|__||__||__|      \___||__|  |__|__|  |__|  |__|__||_____|    |__||__||__|
+
+${NC}"
+
+   printf "$ Zadejte heslo: ${RED}XXX${NC}"
    play -q assets/fail-effect.mp3
 done
+
+clear
+
+echo "${GREEN}● Status: PASSWORD HAS BEEN ACCEPTED${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${RED}
+  ____  __ __  ___ ___   ____  ____   ____  ____       ____  ____ 
+ |    ||  |  ||   |   | /    ||    \ |    ||    |     /    ||    |
+ |__  ||  |  || _   _ ||  o  ||  _  ||__  | |  |     |  o  | |  | 
+ __|  ||  |  ||  \_/  ||     ||  |  |__|  | |  |     |     | |  | 
+/  |  ||  :  ||   |   ||  _  ||  |  /  |  | |  |     |  _  | |  | 
+\  \`  ||     ||   |   ||  |  ||  |  \  \`  | |  |     |  |  | |  | 
+ \____j \__,_||___|___||__|__||__|__|\____||____|    |__|__||____|
+
+${NC}"
+
+sleep 3
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+                                              __  ___   ___ _   __
+  ______ ______ ______ ______ ______ ______  /_ |/ _ \ / _ (_) / /
+ |______|______|______|______|______|______|  | | | | | | | | / / 
+  ______ ______ ______ ______ ______ ______   | | | | | | | |/ /  
+ |______|______|______|______|______|______|  | | |_| | |_| / / _ 
+                                              |_|\___/ \___/_/ (_)
+                                                                  
+                                                                  
+                                                                         
+${NC}"
+
+sleep 0.5
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+                                               ___   ___ _   __
+  ______ ______ ______ ______ ______ ______   / _ \ / _ (_) / /
+ |______|______|______|______|______|______| | (_) | (_) | / / 
+  ______ ______ ______ ______ ______ ______   \__, |\__, |/ /  
+ |______|______|______|______|______|______|    / /   / // / _ 
+                                               /_/   /_//_/ (_)
+                                                               
+                                                                                                                     
+${NC}"
+
+sleep 1.1
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+                                              ___ ______ _   __
+  ______ ______ ______ ______ ______ _____   / _ \____  (_) / /
+ |______|______|______|______|______|_____| | (_) |  / /   / / 
+  ______ ______ ______ ______ ______ _____   \__, | / /   / /  
+ |______|______|______|______|______|_____|    / / / /   / / _ 
+                                              /_/ /_/   /_/ (_)
+                                                                
+                                                                                                                               
+                                                                                                                     
+${NC}"
+sleep 1.7
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+                                             ___ ____ _   __
+  ______ ______ ______ ______ ______ ____   / _ \___ (_) / /
+ |______|______|______|______|______|____| | (_) |__) | / / 
+  ______ ______ ______ ______ ______ ____   \__, |__ < / /  
+ |______|______|______|______|______|____|    / /___) / / _ 
+                                             /_/|____/_/ (_)
+                                                                                                                             
+                                                                                                                     
+${NC}"
+sleep 1.3
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+                                           ___    __ _   __
+  ______ ______ ______ ______ ______ __   / _ \  / /(_) / /
+ |______|______|______|______|______|__| | (_) |/ /_   / / 
+  ______ ______ ______ ______ ______ __   > _ <| '_ \ / /  
+ |______|______|______|______|______|__| | (_) | (_) / / _ 
+                                          \___/ \___/_/ (_)
+                                                               
+                                                                                                                                                                                    
+${NC}"
+sleep 0.8
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+                                       ______ _  _ _   __
+  ______ ______ ______ ______ ______  |____  | || (_) / /
+ |______|______|______|______|______|     / /| || |_ / / 
+  ______ ______ ______ ______ ______     / / |__   _/ /  
+ |______|______|______|______|______|   / /     | |/ / _ 
+                                       /_/      |_/_/ (_)                                                               
+                                                                                                                     
+${NC}"
+sleep 1
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+                                        __ ___  _   __
+  ______ ______ ______ ______ _____    / /|__ \(_) / /
+ |______|______|______|______|_____|  / /_   ) |  / / 
+  ______ ______ ______ ______ _____  | '_ \ / /  / /  
+ |______|______|______|______|_____| | (_) / /_ / / _ 
+                                      \___/____/_/ (_)
+                                                       
+                                                       
+                                                                                                                             
+                                                                                                                     
+${NC}"
+sleep 1.2
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+                                _  _   ___ _   __
+  ______ ______ ______ ______  | || | / _ (_) / /
+ |______|______|______|______| | || || (_) | / / 
+  ______ ______ ______ ______  |__   _> _ < / /  
+ |______|______|______|______|    | || (_) / / _ 
+                                  |_| \___/_/ (_)
+                                                 
+                                                                                                  
+${NC}"
+sleep 0.9
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+                         ____  __ _   __
+  ______ ______ ______  |___ \/_ (_) / /
+ |______|______|______|   __) || |  / / 
+  ______ ______ ______   |__ < | | / /  
+ |______|______|______|  ___) || |/ / _ 
+                        |____/ |_/_/ (_)                                                                                               
+                                                                                                                     
+${NC}"
+sleep 0.7
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+                      ___  _____ _   __
+  ______ ______ ___  |__ \| ____(_) / /
+ |______|______|___|    ) | |__    / / 
+  ______ ______ ___    / /|___ \  / /  
+ |______|______|___|  / /_ ___) |/ / _ 
+                     |____|____//_/ (_)
+                                          
+                                                                                                         
+                                                                                                                     
+${NC}"
+sleep 0.8
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+                  __ ____ _   __
+  ______ ______  /_ |___ (_) / /
+ |______|______|  | | __) | / / 
+  ______ ______   | ||__ < / /  
+ |______|______|  | |___) / / _ 
+                  |_|____/_/ (_)
+                                
+                                                                                                                                                           
+                                                                                                                     
+${NC}"
+sleep 0.9
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+           ______ _   __
+  ______  |____  (_) / /
+ |______|     / /   / / 
+  ______     / /   / /  
+ |______|   / /   / / _ 
+           /_/   /_/ (_)
+                        
+                                                                                                                                                  
+                                                                                                                     
+${NC}"
+sleep 1
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+        ____ _   __
+  ___  |___ (_) / /
+ |___|   __) | / / 
+  ___   |__ < / /  
+ |___|  ___) / / _ 
+       |____/_/ (_)
+                      
+                                                                                                                                              
+                                                                                                                     
+${NC}"
+sleep 1.3
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+      ___  _   __
+  _  |__ \(_) / /
+ |_|    ) |  / / 
+  _    / /  / /  
+ |_|  / /_ / / _ 
+     |____/_/ (_)
+                      
+                                                                                                                                              
+                                                                                                                     
+${NC}"
+sleep 1.8
+
+clear
+
+echo "${BLUE}● Status: SHUTTING DOWN SYSTEM${NC}"
+echo ""
+echo "File: dir,    Node: Top,     This is the TOP SECRET page for controling AT systems."
+echo "This is the Main controlling menu. This terminal has sudo permissions."
+printf "${YELLOW}
+
+   __ _   __
+  /_ (_) / /
+   | |  / / 
+   | | / /  
+   | |/ / _ 
+   |_/_/ (_)
+            
+                                                                           
+                                                                                                                     
+${NC}"
+
+sleep 1
+
+clear
+sleep 1h
